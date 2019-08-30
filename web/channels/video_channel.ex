ab6b8b62f0170454ac85c9790d379d4cadef9e97
joaop21 <jpsilva9898@gmail.com>
2019-08-30 14:18:46 +0100

    handling disconnects

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -3,18 +3,21 @@ defmodule Rumbl.VideoChannel do
 
     alias Rumbl.AnnotationView
 
-    def join("videos:" <> video_id, _params, socket) do
+    def join("videos:" <> video_id, params, socket) do
+        last_seen_id = params["last_seen_id"] || 0
         video_id = String.to_integer(video_id)
         video = Repo.get!(Rumbl.Video, video_id)
+
         annotations = Repo.all(
-            from a in assoc(video, :annotations),
-            order_by: [asc: a.at, asc: a.id],
+                from a in assoc(video, :annotations),
+            where: a.id > ^last_seen_id,
+          order_by: [asc: a.at, asc: a.id],
             limit: 200,
-            preload: [:user]
+          preload: [:user]
         )
 
-        resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView, "annotation.json")}
-
+        resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView,
+                        "annotation.json")}
         {:ok, resp, assign(socket, :video_id, video_id)}
     end
 
--- web/static/js/video.js
+++ web/static/js/video.js
@@ -26,6 +26,7 @@ let Video = {
         })
 
         vidChannel.on("new_annotation", (resp) => {
+            vidChannel.params.last_seen_id = resp.id
             this.renderAnnotation(msgContainer, resp)
         })
 
@@ -34,15 +35,17 @@ let Video = {
             let seconds = e.target.getAttribute("data-seek") ||
                 e.target.parentNode.getAttribute("data-seek")
             if(!seconds){ return }
-            
+
             Player.seekTo(seconds)
         })
 
         vidChannel.join()
-        .receive("ok", resp => {
-            this.scheduleMessages(msgContainer, resp.annotations)
-        })
-        .receive("error", reason => console.log("join failed", reason) )
+            .receive("ok", resp => {
+                let ids = resp.annotations.map(ann => ann.id)
+                if(ids.length > 0){ vidChannel.params.last_seen_id = Math.max(...ids) }
+                this.scheduleMessages(msgContainer, resp.annotations)
+            })
+            .receive("error", reason => console.log("join failed", reason) )
     },
 
     renderAnnotation(msgContainer, {user, body, at}){
