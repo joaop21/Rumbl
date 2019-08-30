0adf4b71ac381f0cfbceda68deec06d828f13781
joaop21 <jpsilva9898@gmail.com>
2019-08-30 11:56:24 +0100

    persisting annotations and click messages

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -16,9 +16,6 @@ let Video = {
         let postButton = document.getElementById("msg-submit")
         let vidChannel = socket.channel("videos:" + videoId)
 
-        vidChannel.join()
-            .receive("ok", resp => console.log("joined the video channel", resp) )
-            .receive("error", reason => console.log("join failed", reason) )
         vidChannel.on("ping", ({count}) => console.log("PING", count) )
 
         postButton.addEventListener("click", e => {
@@ -32,9 +29,57 @@ let Video = {
             this.renderAnnotation(msgContainer, resp)
         })
 
+        msgContainer.addEventListener("click", e => {
+            e.preventDefault()
+            let seconds = e.target.getAttribute("data-seek") ||
+                e.target.parentNode.getAttribute("data-seek")
+            if(!seconds){ return }
+            
+            Player.seekTo(seconds)
+        })
+
         vidChannel.join()
-            .receive("ok", resp => console.log("joined the video channel", resp) )
-            .receive("error", reason => console.log("join failed", reason) )
+        .receive("ok", resp => {
+            this.scheduleMessages(msgContainer, resp.annotations)
+        })
+        .receive("error", reason => console.log("join failed", reason) )
+    },
+
+    renderAnnotation(msgContainer, {user, body, at}){
+        let template = document.createElement("div")
+        template.innerHTML = `
+        <a href="#" data-seek="${this.esc(at)}">
+            [${this.formatTime(at)}]
+            <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+        </a>
+        `
+        msgContainer.appendChild(template)
+        msgContainer.scrollTop = msgContainer.scrollHeight
+    },
+
+    scheduleMessages(msgContainer, annotations){
+        setTimeout(() => {
+            let ctime = Player.getCurrentTime()
+            let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+            this.scheduleMessages(msgContainer, remaining)
+        }, 1000)
+    },
+
+    renderAtTime(annotations, seconds, msgContainer){
+        return annotations.filter( ann => {
+            if(ann.at > seconds){
+                return true
+            } else {
+                this.renderAnnotation(msgContainer, ann)
+                return false
+            }
+        })
+    },
+
+    formatTime(at){
+        let date = new Date(null)
+        date.setSeconds(at / 1000)
+        return date.toISOString().substr(14, 5)
     },
 
     esc(str){
